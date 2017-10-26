cd61e698a66386ba02963161699a2e50a6801edb
oz <taiziozavichay@gmail.com>
2017-10-26 16:47:57 +0300

    fix

---
+++ OfekTwitter/script/user.js
@@ -0,0 +1,10 @@
+var users = [{name:"Oz Taizi",follow:true, id:0},
+			{name:"David Sapiro",follow:true, id:1},
+			{name:"James Bond",follow:true, id:2},
+			{name:"Janis Joplin",follow:true, id:3},
+			{name:"Marty McFly",follow:true, id:4},
+			{name:"Frodo",follow:false, id:5},
+			{name:"Bill Gates",follow:false, id:6},
+			{name:"Janis Joplin",follow:false, id:7},
+			{name:"Mark Twain",follow:false, id:8}];
+
